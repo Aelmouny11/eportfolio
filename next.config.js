d46b8a69f881da1cee0fe8b9afca394e7b81d8e5
aelmouny11 <elmoumny.azddine@gmail.com>
2023-05-18 22:07:25 +0100

    Add 404 page 2

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withNextra = require("nextra")({
 	theme: "nextra-theme-docs",
 	themeConfig: "./theme.config.jsx",
 	latex: true,
-	defaultShowCopyCode:true
+	defaultShowCopyCode: true,
 });
 
 module.exports = withNextra();
